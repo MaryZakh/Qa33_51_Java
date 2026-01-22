81c207c36167fdd72f0738e7ee2d7fe4d6a3ea6a
Maria Z <z0559882272@gmail.com>
2026-01-22 18:25:58 +0200

    Infinity && NaN

--- src/main/java/lesson1/WrapperTypes.java
+++ src/main/java/lesson1/WrapperTypes.java
@@ -2,7 +2,7 @@ package lesson1;
 
 public class WrapperTypes {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         /*
         byte -Byte
         short - Short
@@ -17,57 +17,72 @@ public static void main(String[] args) {
         boolean - Boolean
         */
 
-    int a = 7;
-    //Integer a1 = new Integer(8);
-    Integer a1 = 7;//autopack
-    if (a == a1)
-        System.out.println("equals");//autoUnpack
-    int res = a + a1;//
-    System.out.println(res);
+        int a = 7;
+        //Integer a1 = new Integer(8);
+        Integer a1 = 7;//autopack
+        if (a == a1)
+            System.out.println("equals");//autoUnpack
+        int res = a + a1;//
+        System.out.println(res);
 
-    System.out.println(Integer.toBinaryString(a1));
-    System.out.println(Integer.MIN_VALUE);
-    System.out.println(Integer.MAX_VALUE);
-    System.out.println(Integer.SIZE);
-    System.out.println(Double.SIZE);
-    System.out.println(Double.MIN_VALUE);
-    System.out.println("===================================");
+        System.out.println(Integer.toBinaryString(a1));
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Double.SIZE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println("===================================");
 
-    int a2 = 10, b = 4;
-    Double res2 = div(a2, b);
-    if (res2 != null)
-        System.out.println(res2);
-    else
-        System.out.println("b eql null");
+        int a2 = 10, b = 4;
+        Double res2 = div(a2, b);
+        if (res2 != null)
+            System.out.println(res2);
+        else
+            System.out.println("b eql null");
 
-    Integer[] ints = {1, 2, 3, 4, 5, 6, 0, 9, 100};
-    for (int i = 0; i < ints.length; i++) {
-        // Integer integer = ints[i];
-        System.out.print(ints[i] + " ");
-        System.out.println();
+        Integer[] ints = {1, 2, 3, 4, 5, 6, 0, 9, 100};
+        for (int i = 0; i < ints.length; i++) {
+            // Integer integer = ints[i];
+            System.out.print(ints[i] + " ");
+            System.out.println();
+
+        }
+        //===================================
+        System.out.println("************************");
+        String s = "1.25";
+        System.out.println(s + 2);
+        Double temp = Double.parseDouble(s);//1.25
+        System.out.println(temp + 2);
+        s = "123";
+        Integer temp2 = Integer.parseInt(s);
+        System.out.println(temp2 + 2);
+
+        boolean temp3 = Boolean.parseBoolean("TRUE");//TRUE//True//true//tRUE
+        if (temp3)
+            System.out.println("Hello");
+
+        //========================Continue======================================
+        System.out.println("===================================");
+        double res3 = -3.3 / 1.0;
+        System.out.println(res3);
+        System.out.println(Double.isInfinite(res3));
+
+        res3 = 0.0 / 0.0;
+        System.out.println(res3);
+        System.out.println(Double.isNaN(res3));
+
+
+        //========================================
+
+        System.out.println(Character.isDigit('6'));
+        System.out.println(Character.isDigit('@'));
 
     }
 
-    //===================================
-    System.out.println("************************");
-    String s = "1.25";
-    System.out.println(s + 2);
-    Double temp = Double.parseDouble(s);
-    System.out.println(temp + 2);
-    s = "123";
-    Integer temp2 = Integer.parseInt(s);
-    System.out.println(temp2 + 2);
 
-    boolean temp3 = Boolean.parseBoolean("TRUE");//TRUE//True//true//tRUE
-    if(temp3)
-        System.out.println("Hello");
-
-}
-
-
-private static Double div(int a2, int b) {
-    if (b == 0)
-        return null;
-    return a2 * 1. / b;
-}
+    private static Double div(int a2, int b) {
+        if (b == 0)
+            return null;
+        return a2 * 1. / b;
+    }
 }
